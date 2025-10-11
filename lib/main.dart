8968a70ed55d651cdb9b1699f6d5f2d577644e4b
Mohammed Mohammed Abd El-Fatah <hamoabdelfatah611@gmail.com>
2025-10-12 02:42:34 +0300

    set up Service Locater

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
+import 'package:bookly_app/core/utils/service_locater.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() => runApp(const BooklyApp());
+void main() {
+  setupServiceLocater();
+  runApp(const BooklyApp());
+}
 
 class BooklyApp extends StatelessWidget {
   const BooklyApp({super.key});
