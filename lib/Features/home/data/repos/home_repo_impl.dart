90da24928b89d9e4af413d3a4656b668aa5d25fc
Mohammed Mohammed Abd El-Fatah <hamoabdelfatah611@gmail.com>
2025-10-12 01:20:22 +0300

    feat:fatch api get book

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -32,8 +32,22 @@ class HomeRepoImpl implements HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var data = await apiService.get(
+        endpoint: "volumes?Filtering=free-ebooks &q=subject:programming",
+      );
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServiceFailure.formDioError(e));
+      }
+      return left(ServiceFailure(errMessage: e.toString()));
+    }
   }
 }
