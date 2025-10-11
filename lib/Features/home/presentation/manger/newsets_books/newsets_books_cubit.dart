35393e069cad23fa6b65b619d6e586f0c7ad21e4
Mohammed Mohammed Abd El-Fatah <hamoabdelfatah611@gmail.com>
2025-10-12 02:03:04 +0300

    create cubit for new set book

---
+++ lib/Features/home/presentation/manger/newsets_books/newsets_books_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'newsets_books_state.dart';
+
+class NewSetBooksCubit extends Cubit {
+  NewSetBooksCubit(this.homeRepo) : super(NewSetBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewSetBooks() async {
+    emit(NewSetBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) {
+        emit(NewSetBooksFailure(failure.errMessage));
+      },
+      (books) {
+        emit(NewSetBooksSuccess(books));
+      },
+    );
+  }
+}
---
+++ lib/Features/home/presentation/manger/newsets_books/newsets_books_state.dart
@@ -0,0 +1,24 @@
+part of 'newsets_books_cubit.dart';
+
+abstract class NewSetsBooksState extends Equatable {
+  const NewSetsBooksState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class NewSetBooksLoading extends NewSetsBooksState {}
+
+class NewSetBooksInitial extends NewSetsBooksState {}
+
+class NewSetBooksFailure extends NewSetsBooksState {
+  final String errMessage;
+
+  const NewSetBooksFailure(this.errMessage);
+}
+
+class NewSetBooksSuccess extends NewSetsBooksState {
+  final List<BookModel> books;
+
+  const NewSetBooksSuccess(this.books);
+}
